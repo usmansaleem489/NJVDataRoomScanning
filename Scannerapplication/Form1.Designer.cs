935c50fc35861cb3bb0014908df2700cc94ed538
Muhammad Usman <39616732+usmansaleem489@users.noreply.github.com>
2021-12-01 17:32:24 +0500

    Scanning is done, Profile Picture scanning remains

---
+++ Scannerapplication/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿using System.Windows.Forms;
+namespace Scannerapplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showImages = new System.Windows.Forms.Button();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.pnl_capture = new System.Windows.Forms.Panel();
+            this.pic_scan = new System.Windows.Forms.PictureBox();
+            this.lbDevices = new System.Windows.Forms.ListBox();
+            this.btn_scan = new System.Windows.Forms.Button();
+            this.pnl_capture.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_scan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // showImages
+            // 
+            this.showImages.Location = new System.Drawing.Point(242, 216);
+            this.showImages.Name = "showImages";
+            this.showImages.Size = new System.Drawing.Size(85, 23);
+            this.showImages.TabIndex = 5;
+            this.showImages.Text = "Show Images";
+            this.showImages.UseVisualStyleBackColor = true;
+            this.showImages.Visible = false;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(787, 3);
+            this.splitter1.TabIndex = 38;
+            this.splitter1.TabStop = false;
+            // 
+            // pnl_capture
+            // 
+            this.pnl_capture.BackColor = System.Drawing.Color.Transparent;
+            this.pnl_capture.Controls.Add(this.pic_scan);
+            this.pnl_capture.Controls.Add(this.lbDevices);
+            this.pnl_capture.Controls.Add(this.btn_scan);
+            this.pnl_capture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_capture.Location = new System.Drawing.Point(0, 3);
+            this.pnl_capture.Name = "pnl_capture";
+            this.pnl_capture.Size = new System.Drawing.Size(787, 712);
+            this.pnl_capture.TabIndex = 39;
+            // 
+            // pic_scan
+            // 
+            this.pic_scan.BackColor = System.Drawing.Color.White;
+            this.pic_scan.Location = new System.Drawing.Point(165, 21);
+            this.pic_scan.Name = "pic_scan";
+            this.pic_scan.Size = new System.Drawing.Size(450, 600);
+            this.pic_scan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_scan.TabIndex = 6;
+            this.pic_scan.TabStop = false;
+            this.pic_scan.Visible = false;
+            // 
+            // lbDevices
+            // 
+            this.lbDevices.FormattingEnabled = true;
+            this.lbDevices.Location = new System.Drawing.Point(21, 53);
+            this.lbDevices.Name = "lbDevices";
+            this.lbDevices.Size = new System.Drawing.Size(120, 30);
+            this.lbDevices.TabIndex = 5;
+            this.lbDevices.Visible = false;
+            // 
+            // btn_scan
+            // 
+            this.btn_scan.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_scan.Location = new System.Drawing.Point(21, 17);
+            this.btn_scan.Name = "btn_scan";
+            this.btn_scan.Size = new System.Drawing.Size(75, 30);
+            this.btn_scan.TabIndex = 4;
+            this.btn_scan.Text = "Scan";
+            this.btn_scan.UseVisualStyleBackColor = true;
+            this.btn_scan.Click += new System.EventHandler(this.btn_scan_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(787, 715);
+            this.Controls.Add(this.pnl_capture);
+            this.Controls.Add(this.splitter1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+
+            this.SizeChanged += new System.EventHandler(this.Home_SizeChanged);
+
+            this.Text = "Home";
+            this.pnl_capture.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_scan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+
+
+        //camera
+        private System.Windows.Forms.Button showImages;
+        private Splitter splitter1;
+        private Panel pnl_capture;
+        private PictureBox pic_scan;
+        private ListBox lbDevices;
+        private Button btn_scan;
+    }
+}
+
